08418844753f6c7210c051ea18b9bc475c4b1f7f
kkunal1408 <kkunal1408@gmail.com>
2022-11-19 12:06:37 -0800

    resnet50

--- quantization_functions/quant_aware_resnet_model.py
+++ quantization_functions/quant_aware_resnet_model.py
@@ -80,85 +80,85 @@ class CBasicBlock(nn.Module):
         if self.downsample:
             self.downsample.quantize(if_quantize)
 
-# class CBottleneckBlock(nn.Module):
-#     expansion = 4
+class CBottleneckBlock(nn.Module):
+    expansion = 4
 
-#     def __init__(self, inplanes, outplanes, q_num_bit, layer_name="", qat=False):
-#         super(CBottleneckBlock, self).__init__()
-#         self.inplanes = inplanes
-#         self.outplanes = outplanes
-#         stride = outplanes*4 // inplanes
-#         downsample = stride >= 2
-#         if stride >2: stride=1
+    def __init__(self, inplanes, outplanes, q_num_bit, layer_name="", qat=False):
+        super(CBottleneckBlock, self).__init__()
+        self.inplanes = inplanes
+        self.outplanes = outplanes
+        stride = outplanes*4 // inplanes
+        downsample = stride >= 2
+        if stride >2: stride=1
 
-#         self.layer_name = layer_name
+        self.layer_name = layer_name
 
-#         state_dict_names1 = [layer_name + '.' + name for name in
-#                              ['conv1.weight', "", 'bn1.weight', 'bn1.bias', 'bn1.running_mean', 'bn1.running_var',
-#                               'bn1.num_batches_tracked']]
-#         self.conv1 = CConvBNReLU2d(
-#             inplanes, outplanes, (1, 1), stride, padding=0, bias=False, dilation=1,
-#             q_num_bit=q_num_bit, affine=True, relu=True, state_dict_names=state_dict_names1, qat=qat
-#         )
+        state_dict_names1 = [layer_name + '.' + name for name in
+                             ['conv1.weight', "", 'bn1.weight', 'bn1.bias', 'bn1.running_mean', 'bn1.running_var',
+                              'bn1.num_batches_tracked']]
+        self.conv1 = CConvBNReLU2d(
+            inplanes, outplanes, (1, 1), stride, padding=0, bias=False, dilation=1,
+            q_num_bit=q_num_bit, affine=True, relu=True, state_dict_names=state_dict_names1, qat=qat
+        )
 
-#         state_dict_names2 = [layer_name + '.' + name for name in
-#                              ['conv2.weight', "", 'bn2.weight', 'bn2.bias', 'bn2.running_mean', 'bn2.running_var',
-#                               'bn2.num_batches_tracked']]
-#         self.conv2 = CConvBNReLU2d(
-#             outplanes, outplanes, (3, 3), (1, 1), padding=1, bias=False, dilation=1,
-#             q_num_bit=q_num_bit, affine=True, relu=True, state_dict_names=state_dict_names2, qat=qat
-#         )
+        state_dict_names2 = [layer_name + '.' + name for name in
+                             ['conv2.weight', "", 'bn2.weight', 'bn2.bias', 'bn2.running_mean', 'bn2.running_var',
+                              'bn2.num_batches_tracked']]
+        self.conv2 = CConvBNReLU2d(
+            outplanes, outplanes, (3, 3), (1, 1), padding=1, bias=False, dilation=1,
+            q_num_bit=q_num_bit, affine=True, relu=True, state_dict_names=state_dict_names2, qat=qat
+        )
 
-#         state_dict_names3 = [layer_name + '.' + name for name in
-#                              ['conv3.weight', "", 'bn3.weight', 'bn3.bias', 'bn3.running_mean', 'bn3.running_var',
-#                               'bn3.num_batches_tracked']]
-#         self.conv3 = CConvBNReLU2d(
-#             outplanes, outplanes*4, (1, 1), (1, 1), padding=0, bias=False, dilation=1,
-#             q_num_bit=q_num_bit, affine=True, relu=False, state_dict_names=state_dict_names3, qat=qat
-#         )
+        state_dict_names3 = [layer_name + '.' + name for name in
+                             ['conv3.weight', "", 'bn3.weight', 'bn3.bias', 'bn3.running_mean', 'bn3.running_var',
+                              'bn3.num_batches_tracked']]
+        self.conv3 = CConvBNReLU2d(
+            outplanes, outplanes*4, (1, 1), (1, 1), padding=0, bias=False, dilation=1,
+            q_num_bit=q_num_bit, affine=True, relu=False, state_dict_names=state_dict_names3, qat=qat
+        )
 
-#         self.act2 = nn.ReLU(inplace=True)
-#         self.stride = stride
+        self.act2 = nn.ReLU(inplace=True)
+        self.stride = stride
 
-#         if downsample:
-#             state_dict_names_d = [layer_name + '.' + name for name in
-#                                   ['downsample.0.weight', "", 'downsample.1.weight', 'downsample.1.bias',
-#                                    'downsample.1.running_mean', 'downsample.1.running_var',
-#                                    'downsample.1.num_batches_tracked']]
-#             self.downsample = CConvBNReLU2d(
-#                 inplanes, outplanes*4, kernel_size=(1, 1), stride=stride, bias=False,
-#                 q_num_bit=q_num_bit, affine=True, relu=False, state_dict_names=state_dict_names_d, qat=qat
-#             )
-#         else:
-#             self.downsample = None
-#         self.add = CAdd(q_num_bit=q_num_bit, qat=qat)
-#         self.act2 = nn.ReLU()
+        if downsample:
+            state_dict_names_d = [layer_name + '.' + name for name in
+                                  ['downsample.0.weight', "", 'downsample.1.weight', 'downsample.1.bias',
+                                   'downsample.1.running_mean', 'downsample.1.running_var',
+                                   'downsample.1.num_batches_tracked']]
+            self.downsample = CConvBNReLU2d(
+                inplanes, outplanes*4, kernel_size=(1, 1), stride=stride, bias=False,
+                q_num_bit=q_num_bit, affine=True, relu=False, state_dict_names=state_dict_names_d, qat=qat
+            )
+        else:
+            self.downsample = None
+        self.add = CAdd(q_num_bit=q_num_bit, qat=qat)
+        self.act2 = nn.ReLU()
 
-#     def load_pretrained(self, state_dict):
-#         self.conv1.load_pretrained(state_dict)
-#         self.conv2.load_pretrained(state_dict)
-#         self.conv3.load_pretrained(state_dict)
-#         if self.downsample:
-#             self.downsample.load_pretrained(state_dict)
+    def load_pretrained(self, state_dict):
+        self.conv1.load_pretrained(state_dict)
+        self.conv2.load_pretrained(state_dict)
+        self.conv3.load_pretrained(state_dict)
+        if self.downsample:
+            self.downsample.load_pretrained(state_dict)
 
-#     def forward(self, x):
-#         shortcut = x
-#         x = self.conv1(x)
-#         x = self.conv2(x)
-#         x = self.conv3(x)
-#         if self.downsample:
-#             shortcut = self.downsample(shortcut)
-#         x = self.add(x, shortcut)
-#         x = self.act2(x)
-#         return x
+    def forward(self, x):
+        shortcut = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        if self.downsample:
+            shortcut = self.downsample(shortcut)
+        x = self.add(x, shortcut)
+        x = self.act2(x)
+        return x
 
-#     def quantize(self, if_quantize):
-#         self.conv1.quantize(if_quantize)
-#         self.conv2.quantize(if_quantize)
-#         self.conv3.quantize(if_quantize)
-#         self.add.quantize(if_quantize)
-#         if self.downsample:
-#             self.downsample.quantize(if_quantize)
+    def quantize(self, if_quantize):
+        self.conv1.quantize(if_quantize)
+        self.conv2.quantize(if_quantize)
+        self.conv3.quantize(if_quantize)
+        self.add.quantize(if_quantize)
+        if self.downsample:
+            self.downsample.quantize(if_quantize)
 
 
 class CResnet(nn.Module):
@@ -234,7 +234,7 @@ def _resnet_builder(num_class, q_num_bit, block_type, layers, pretrained=True, n
         else:
             with open(pretrained, 'rb') as f:
                 state_dict = torch.load(f, map_location='cpu')
-        print(q_num_bit)
+        # print(q_num_bit)
         model.conv1.load_pretrained(state_dict, q_num_bit)
         for layer in [model.layer1, model.layer2, model.layer3, model.layer4]:
             for block in layer:
@@ -251,6 +251,39 @@ def _resnet_builder(num_class, q_num_bit, block_type, layers, pretrained=True, n
 
     return model
 
+
+# class CSVM(nn.Module):
+#     def __init__(self, num_class, q_num_bit, qat=False):
+#         super(CSVM, self).__init__()
+#         self.batch_size = 64
+#         input_size = 784  # (dimension of image 28 * 28)
+#         self.svm_model = CLinear(
+#             input_size, num_class, q_num_bit=q_num_bit, qat=True)
+
+#     def forward(self, x, labels):
+#         x = self.svm_model(x)
+#         x = torch.sum(torch.clamp(1 - x.t()*labels, min=0))/self.batch_size
+#         return x
+
+#     def quantize(self, if_quantize):
+#         self.svm_model.quantize(if_quantize)
+
+# def _svm_builder(num_class, q_num_bit, pretrained=True, name='SVM', qat=False):
+
+#     model = CSVM(num_class, q_num_bit, qat=qat)
+
+#     if pretrained:
+#         if pretrained is True:
+#             import timm
+#             state_dict = timm.create_model(name, pretrained=True).state_dict()
+#         else:
+#             with open(pretrained, 'rb') as f:
+#                 state_dict = torch.load(f, map_location='cpu')
+#         model.svm_model.load_pretrained(state_dict, q_num_bit)
+#         print('remained state dict', state_dict.keys())
+
+#     return model
+
 def CResnet18(num_class, q_num_bit, pretrained=True, qat=False):
 
     return _resnet_builder(num_class, q_num_bit, CBasicBlock, [2,2,2,2], pretrained, 'resnet18', qat=qat)
@@ -259,9 +292,9 @@ def CResnet18(num_class, q_num_bit, pretrained=True, qat=False):
 
 #     return _resnet_builder(num_class, q_num_bit, CBasicBlock, [3,4,6,3], pretrained, 'resnet34', qat=qat)
 
-# def CResnet50(num_class, q_num_bit, pretrained=True, qat=False):
+def CResnet50(num_class, q_num_bit, pretrained=True, qat=False):
 
-#     return _resnet_builder(num_class, q_num_bit, CBottleneckBlock, [3,4,6,3], pretrained, 'resnet50', qat=qat)
+    return _resnet_builder(num_class, q_num_bit, CBottlneckBlock, [3,4,6,3], pretrained, 'resnet50', qat=qat)
 
 # def CResnet101(num_class, q_num_bit, pretrained=True, qat=False):
 
@@ -270,3 +303,7 @@ def CResnet18(num_class, q_num_bit, pretrained=True, qat=False):
 # def CResnet152(num_class, q_num_bit, pretrained=True, qat=False):
 
 #     return _resnet_builder(num_class, q_num_bit, CBottleneckBlock, [3,8,36,3], pretrained, 'resnet152', qat=qat)
+
+
+# def CSVM8(num_class, q_num_bit, pretrained=True, qat=False):
+#     return _svm_builder(num_class, q_num_bit, pretrained, 'resnet18', qat=qat)
